7e9a40efef3eafddc0a4288896a71f5a4d92d56a
HarukiKumagai <biwakox@gmail.com>
2024-08-25 17:10:48 +0900

    ifの条件を正規表現を使用したものへ変更

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,16 +16,7 @@ public class Main {
 
     System.out.print("携帯電話番号を入力してください: ");
     String phoneNumber = scanner.next();
-    String[] numberParts = phoneNumber.split("-"); // ハイフンで配列を区切る
-    if (numberParts.length == 3 && // 区切られた配列が3つに分割されている　かつ
-        numberParts[0].length() == 3 && // 1つ目の配列が3桁　かつ
-        numberParts[1].length() == 4 && // 2つ目の配列が4桁　かつ
-        numberParts[2].length() == 4 && // 3つ目の配列が4桁　かつ
-        (numberParts[0].equals("070") || // 1つ目の配列が"070", "080", "090" のいずれかである　かつ
-            numberParts[0].equals("080") ||
-            numberParts[0].equals("090")) &&
-        numberParts[1].matches("^[0-9]+$") && // 2つ目、3つ目の配列が数字のみで構成されている場合true
-        numberParts[2].matches("^[0-9]+$")) {
+    if (phoneNumber.matches("^(070|080|090)-\\d{4}-\\d{4}$")) {
       System.out.println(phoneNumber + "は有効な携帯電話番号です。");
     } else {
       System.out.println(phoneNumber + "は無効な携帯電話番号です。");
